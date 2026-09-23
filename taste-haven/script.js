97c8206a3e3ac2406d1b4d6c198355f256436eb1
ucheokoro001 <okorouche323@gmail.com>
2026-09-23 22:28:05 +0100

    Add files via upload

---
+++ taste-haven/script.js
@@ -0,0 +1,9 @@
+// MOBILE MENU
+
+const menuBtn = document.getElementById("menuBtn");
+const navLinks = document.getElementById("navLinks");
+
+menuBtn.addEventListener("click", function () {
+    navLinks.classList.toggle("active");
+});
+
